d16214584291ad123ac9f8b649af794034e75740
Cesar MC <cesarmoracastro@gmail.com>
2013-06-06 20:18:39 -0500

    Working a little bit in AuraEngineIOS

--- source/Engine/IOS/AuraEngineIOS.h
+++ source/Engine/IOS/AuraEngineIOS.h
@@ -24,81 +24,14 @@ namespace Aura{
   {
   public:
     
-  AuraEngineIOS(): AuraEngine(){
-      mGestureView = 0;
+    AuraEngineIOS();
 
-    }
-
-
-    virtual void init( )
-    {
-      createRoot();
-      Ogre::String nextRenderer = mRoot->getAvailableRenderers()[0]->getName();
-      mRoot->setRenderSystem(mRoot->getRenderSystemByName(nextRenderer));
-      //setup();
-      Ogre::Root::getSingleton().getRenderSystem()->_initRenderTargets();
-      Ogre::Root::getSingleton().clearEventTimes();
-    }
-
-
-    virtual void createWindow()
-      {
-	mWindow = mRoot->initialise(true, "Aura Application");
-	mGestureView = [[AuraApplicationGestureView alloc] init];
-	mGestureView.mEngine = this;
-	[[[UIApplication sharedApplication] keyWindow] addSubview:mGestureView];
-	
-      }
-
-
+    virtual void init();
+    virtual void createWindow();
+    
   private:
     AuraApplicationGestureView *mGestureView;
-
   };
 } // Aura
-
-
-
-/* Iphone  Objective C binding */
-@implementation AuraApplicationGestureView
-
-@synthesize mEngine;
-
-- (BOOL)canBecomeFirstResponder
-{
-  return YES;
-}
-
-- (void)dealloc {
-  [super dealloc];
-}
-
-- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-  if(mEngine && event.type == UIEventTypeMotion && event.subtype == UIEventSubtypeMotionShake)
-    mEngine->mAuraApp->motionBegan();
-        
-  if ([super respondsToSelector:@selector(motionBegan:withEvent:)]) {
-    [super motionBegan:motion withEvent:event];
-  }
-}
-
-- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-  if(mEngine && event.type == UIEventTypeMotion && event.subtype == UIEventSubtypeMotionShake)
-    mEngine->mAuraApp->motionEnded();
-        
-  if ([super respondsToSelector:@selector(motionEnded:withEvent:)]) {
-    [super motionEnded:motion withEvent:event];
-  }
-}
-
-- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-  if(mEngine && event.type == UIEventTypeMotion && event.subtype == UIEventSubtypeMotionShake)
-    mEngine->mAuraApp->motionCancelled();
-        
-  if ([super respondsToSelector:@selector(motionCancelled:withEvent:)]) {
-    [super motionCancelled:motion withEvent:event];
-  }
-}
-@end
 
 #endif //_AuraOgreEngineIOS_H_
